ef3a39a7794ae36ec41ff28fd20e93ca9fb3896c
mommy79 <m9472757c@gmail.com>
2018-04-10 16:29:51 +0900

    [Feature]Refactoring stack_image_node.py

--- src/stack_image_node/src/stack_image_node.py
+++ src/stack_image_node/src/stack_image_node.py
@@ -9,7 +9,7 @@ import cv2
 FILE_COUNT = 0
 
 
-def test_callback(msg):
+def image_callback(msg):
     print("[INFO]Received an image!")
 
     global FILE_COUNT
@@ -28,34 +28,9 @@ def test_callback(msg):
         FILE_COUNT += 1
 
 
-def image_callback(msg):
-    print("[INFO]Received an image!")
-    try:
-        cvb = CvBridge()
-        cv2_img = cvb.imgmsg_to_cv2(msg, "bgr8")
-        cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
-    except CvBridgeError, e:
-        print(e)
-    else:
-        global cnt
-        if cnt % 5 == 0:
-            print("[INFO]Saving an image!")
-
-            f = open('../data/count.txt', 'r')
-            count = f.readline()
-            f.close()
-
-            cv2.imwrite('../data/sample{}.png'.format(count), cv2_img)
-
-            f = open('../data/count.txt', 'w')
-            f.write(str(int(count) + 1))
-            f.close()
-        cnt += 1
-
-
 def main():
     rospy.init_node('Camera_Image_Saver')
-    rospy.Subscriber('image_raw', Image, test_callback, queue_size=5)
+    rospy.Subscriber('image_raw', Image, image_callback, queue_size=5)
     rospy.spin()
 
 
